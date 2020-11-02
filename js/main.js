36095f2b72a78afa95c4637357acbaa51d01cce5
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2020-11-02 10:28:49 +0100

    feat:animation on scroll

--- js/main.js
+++ js/main.js
@@ -3,3 +3,30 @@ window.addEventListener('load', () => {
     const pageLoader = document.querySelector('.page-loader');
     pageLoader.classList.add('hidden');    
 })
+
+// animation on scroll
+
+const fadeIns = document.querySelectorAll(".fadein-onscroll")
+
+// intersection observer function
+
+const appearOptions = {
+    threshold: 0.5
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(entries, appearOnScroll) {
+
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+fadeIns.forEach(fadeIn => {
+    appearOnScroll.observe(fadeIn)
+})
